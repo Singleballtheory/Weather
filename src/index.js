91508836d7061f5a42df0faa8050f4ba31a9841b
Scott Hutley <scotthutley1@comcast.net>
2021-09-07 11:50:58 -0700

    Add two day forecast
    Co-authored by Ben Dunham <bendunhampdx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     $('#location').val("");
 
     let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.API_KEY}`;
+    const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${process.env.API_KEY}`;
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
@@ -22,8 +22,19 @@ $(document).ready(function() {
     request.send();
 
     function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in fahrenheit is ${response.main.temp} degrees.`);
+      $('.time').text(`The forcast for ${city} on ${response.list[0].dt_txt}`);
+      $('.showHumidity').text(`The humidity in ${city} is ${response.list[0].main.humidity}%`);
+      $('.showTemp').text(`The temperature in fahrenheit is ${response.list[0].main.temp} degrees.`);
+      $('.showVis').text(`The visibility in feet is ${response.list[0].visibility}.`);
+      $('.showClouds').text(`The cloud coverage today is ${response.list[0].clouds.all}%.`);
+      
+      $('.time1').text(`The forcast for ${city} on ${response.list[8].dt_txt}`);
+      $('.showHumidity1').text(`The humidity in ${city} is ${response.list[8].main.humidity}%`);
+      $('.showTemp1').text(`The temperature in fahrenheit is ${response.list[8].main.temp} degrees.`);
+      $('.showVis1').text(`The visibility in feet is ${response.list[8].visibility}.`);
+      $('.showClouds1').text(`The cloud coverage today is ${response.list[8].clouds.all}%.`);
     }
   });
 });
+
+
