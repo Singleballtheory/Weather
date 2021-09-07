e9a75c57cb1aa904bbd518df0d25a2ca7f844a80
Scott Hutley <scotthutley1@comcast.net>
2021-09-07 15:00:23 -0700

    Remove visibility API calls, update submit button text
    Co-authored by Ben Dunham <bendunhampdx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -30,22 +30,19 @@ $(document).ready(function() {
 
       // $('.main').text(result);
       $('.time').text(`The forcast for ${city} on ${response.list[2].dt_txt}`);
-      $('.showHumidity').text(`The humidity in ${city} is ${response.list[2].main.humidity}%`);
-      $('.showTemp').text(`The temperature in fahrenheit is ${response.list[2].main.temp} degrees.`);
-      $('.showVis').text(`The visibility in feet is ${response.list[2].visibility}.`);
-      $('.showClouds').text(`The cloud coverage today is ${response.list[2].clouds.all}%.`);
+      $('.showHumidity').text(`The humidity in ${city} will be ${response.list[2].main.humidity}%`);
+      $('.showTemp').text(`The temperature in fahrenheit will be ${response.list[2].main.temp} degrees.`);
+      $('.showClouds').text(`The cloud coverage today will be ${response.list[2].clouds.all}%.`);
       
       $('.time1').text(`The forcast for ${city} on ${response.list[10].dt_txt}`);
-      $('.showHumidity1').text(`The humidity in ${city} is ${response.list[10].main.humidity}%`);
-      $('.showTemp1').text(`The temperature in fahrenheit is ${response.list[10].main.temp} degrees.`);
-      $('.showVis1').text(`The visibility in feet is ${response.list[10].visibility}.`);
-      $('.showClouds1').text(`The cloud coverage today is ${response.list[10].clouds.all}%.`);
+      $('.showHumidity1').text(`The humidity in ${city} will be ${response.list[10].main.humidity}%`);
+      $('.showTemp1').text(`The temperature in fahrenheit will be ${response.list[10].main.temp} degrees.`);
+      $('.showClouds1').text(`The cloud coverage today will be ${response.list[10].clouds.all}%.`);
 
       $('.time2').text(`The forcast for ${city} on ${response.list[18].dt_txt}`);
-      $('.showHumidity2').text(`The humidity in ${city} is ${response.list[18].main.humidity}%`);
-      $('.showTemp2').text(`The temperature in fahrenheit is ${response.list[18].main.temp} degrees.`);
-      $('.showVis2').text(`The visibility in feet is ${response.list[18].visibility}.`);
-      $('.showClouds2').text(`The cloud coverage today is ${response.list[18].clouds.all}%.`);
+      $('.showHumidity2').text(`The humidity in ${city} will be ${response.list[18].main.humidity}%`);
+      $('.showTemp2').text(`The temperature in fahrenheit will be ${response.list[18].main.temp} degrees.`);
+      $('.showClouds2').text(`The cloud coverage today will be ${response.list[18].clouds.all}%.`);
     }
   });
 });
