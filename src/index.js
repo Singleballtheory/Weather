7d40b2f8ddae46357194921bef1974e1ca8d7324
Scott Hutley <scotthutley1@comcast.net>
2021-09-08 07:41:54 -0700

    Add css elements, update text in multiple display elements
    Co-authored by Ben Dunham <bendunhampdx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ function checkNumber(zipcode) {
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
+    $('.col-md-4').show();
     const zipcode = parseInt($('#location').val());
     $('#location').val("");
 
@@ -23,10 +24,11 @@ $(document).ready(function() {
         throw RangeError("Please enter a valid zip-code!");
       } else {
         console.log("Try was successful, so no need to catch!");
-        $('#displayNumber').text("This number is valid. You may continue.");
+        $('#displayNumber').text("Thank you for selecting a valid zip-code. Here is the three day forecast.");
       }
     } catch(error) {
       $('.showErrors').text(`Red alert! We have an error: ${error.message}`);
+      
     }
 
     let request = new XMLHttpRequest();
@@ -51,19 +53,20 @@ $(document).ready(function() {
 
       // $('.main').text(result);
       $('.time').text(`The forcast for ${zipcode} on ${response.list[2].dt_txt}`);
-      $('.showHumidity').text(`The humidity in ${zipcode} will be ${response.list[2].main.humidity}%`);
-      $('.showTemp').text(`The temperature in fahrenheit will be ${response.list[2].main.temp} degrees.`);
+      $('.showHumidity').text(`The humidity in ${zipcode} will be ${response.list[2].main.humidity}%.`);
+      $('.showTemp').text(`The temperature will be ${response.list[2].main.temp} degrees.`);
       $('.showClouds').text(`The cloud coverage today will be ${response.list[2].clouds.all}%.`);
       
       $('.time1').text(`The forcast for ${zipcode} on ${response.list[10].dt_txt}`);
-      $('.showHumidity1').text(`The humidity in ${zipcode} will be ${response.list[10].main.humidity}%`);
-      $('.showTemp1').text(`The temperature in fahrenheit will be ${response.list[10].main.temp} degrees.`);
+      $('.showHumidity1').text(`The humidity in ${zipcode} will be ${response.list[10].main.humidity}%.`);
+      $('.showTemp1').text(`The temperature will be ${response.list[10].main.temp} degrees.`);
       $('.showClouds1').text(`The cloud coverage today will be ${response.list[10].clouds.all}%.`);
 
       $('.time2').text(`The forcast for ${zipcode} on ${response.list[18].dt_txt}`);
-      $('.showHumidity2').text(`The humidity in ${zipcode} will be ${response.list[18].main.humidity}%`);
-      $('.showTemp2').text(`The temperature in fahrenheit will be ${response.list[18].main.temp} degrees.`);
+      $('.showHumidity2').text(`The humidity in ${zipcode} will be ${response.list[18].main.humidity}%.`);
+      $('.showTemp2').text(`The temperature will be ${response.list[18].main.temp} degrees.`);
       $('.showClouds2').text(`The cloud coverage today will be ${response.list[18].clouds.all}%.`);
+      
     }
   });
 });
